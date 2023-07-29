0fd94a7ab985e9d597bb00c2a1fca20565362251
Kyle Kailihiwa <kylekailihiwa@gmail.com>
2023-07-28 23:12:22 -0700

    Finally putting the Vita specific project up

---
+++ Levels/End.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _ready():
+	$Button.grab_focus()
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("toggle_fullscreen"):
+		OS.window_fullscreen = !OS.window_fullscreen
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://Title Screen/Title Screen.tscn")
---
+++ Title Screen/Title Screen.gd
@@ -0,0 +1,29 @@
+extends Control
+
+#onready var quitButton = $Quit
+
+func _ready():
+	if !MusicController.IsMusicPlaying():
+		MusicController.PlayMusic("res://Sounds/Music/Title Screen.ogg")
+	
+	$"Start Game".grab_focus()
+	
+	print(OS.get_name())
+	
+	#if OS.get_name() == "HTML5":
+	#	quitButton.disabled = true
+	#	quitButton.visible = false
+
+func _on_Quit_pressed():
+	get_tree().quit()
+
+func _on_Controls_pressed():
+	get_tree().change_scene("res://Title Screen/Controls/Controls.tscn")
+	print("Changed scene")
+
+func _on_Settings_pressed():
+	get_tree().change_scene("res://Title Screen/Settings/Settings.tscn")
+
+func _on_Start_Game_pressed():
+	MusicController.StopMusic()
+	get_tree().change_scene("res://Levels/Level 1/Level 1.tscn")
